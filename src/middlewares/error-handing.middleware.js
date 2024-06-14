942de7f5aa7dbe8349199f67c0a511f5ad7ae047
jkc-mycode <jkc9709@naver.com>
2024-06-14 15:05:24 +0900

    Fix/auth controller, service

--- src/middlewares/error-handing.middleware.js
+++ src/middlewares/error-handing.middleware.js
@@ -19,17 +19,7 @@ export default (err, req, res, next) => {
     }
 
     // HttpError 클래스의 에러 처리
-    // err instanceof HttpError.BadRequest는 err 객체가
-    // HttpError.BadRequest 클래스의 인스턴스인지 확인하는 표현
-    // 에러처리 미들웨어에서는 throw new HttpError 클래스 사용 안하게 좋음 => 루한루프에 빠질 수도 있어서
-    if (
-        err instanceof HttpError.BadRequest ||
-        err instanceof HttpError.Unauthorized ||
-        err instanceof HttpError.Forbidden ||
-        err instanceof HttpError.NotFound ||
-        err instanceof HttpError.Conflict ||
-        err instanceof HttpError.InternalServerError
-    ) {
+    if (err.status && err.message) {
         return res.status(err.status).json({
             status: err.status,
             message: err.message,
